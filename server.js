5b4b9dab76af59472d497d161618bf6f5716462b
sirferra <sir.leandroferra@gmail.com>
2021-02-01 23:00:33 -0300

    Update readme, add npm script commands

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 //const db = require('./src/sql/conectionDB');
-require('./src/sql/conectionDB').createTables();
 //require('./src/sql/conectionDB').insertTables();;
 
 // routers
