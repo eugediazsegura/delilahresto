3cb8922ebf7fbd456c72ebea67c38bfe578b97d9
Euge <eugeniaa.ds@gmail.com>
2021-02-02 21:07:35 -0300

    Readme y cambio de variables

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+global.db = {
+    DB_HOST:'127.0.0.1',
+    DB_USER:'root',
+    DB_PASS:'1234',
+    DB_DATABASE:'delilah_resto'
+}
+//const db = require('./src/sql/conectionDB');
+//require('./src/sql/conectionDB').insertTables();;
 
 // routers
 const usersRouter = require('./src/routes/users');
